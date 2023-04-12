10cbc8fa9ae3161aedd1e61e95193ddb06a3f8f2
Lisa <45350703+brbiegrl@users.noreply.github.com>
2023-04-12 23:43:04 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,10 +5,10 @@ import numpy as np
 chat_id = 909631698 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, x_cnt: int, y_success: int, y_cnt: int) -> bool:
-  count = np.array([x_success, y_success])
-  nobs = np.array([x_cnt, y_cnt])
-  value = 0.02
-  stat, pval = proportions_ztest(count, nobs, value)
-  if round(pval) == 1:
-    return False
-  return True
+    count = [x_success, y_success]
+    nobs = [x_cnt, y_cnt]
+    value = 0.02
+    stat, pval = proportions_ztest(count, nobs, value)
+    if round(pval) == 1:
+        return False
+    return True
