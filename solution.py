0006aaa1a95ed0de7c24481a69eea4c2e01451a7
Lisa <45350703+brbiegrl@users.noreply.github.com>
2023-04-12 23:36:56 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,13 +2,13 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 909631698 # Ваш chat ID, не меняйте название переменной
 
-def solution(x_success: int, 
-             x_cnt: int, 
-             y_success: int, 
-             y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+def solution(x_success: int, x_cnt: int, y_success: int, y_cnt: int) -> bool:
+  count = np.array([x_success, y_success])
+  nobs = np.array([x_cnt, y_cnt])
+  value = 0.02
+  stat, pval = proportions_ztest(count, nobs, value)
+  if round(pval) == 1:
+    return False
+  return True
